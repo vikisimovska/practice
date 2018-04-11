c1cac7de34d60506f6dda87faac6934e5fb823c6
Violeta Simovska <vikisimovska27@gmail.com>
2018-04-11 13:00:11 -0700

    version2

--- practice.js
+++ practice.js
@@ -1,5 +1,6 @@
 let arr = [1, 2, 3, 4, 5];
 
+//return 1, 2, 3, ..
 function printForward(arr, i = 0) {
   if (i === arr.length) return;
 
@@ -16,3 +17,13 @@ function printRecursive(arr, i = 0) {
 
 printForward(arr);
 printRecursive(arr);
+
+//***********2 version********************
+
+//return an ARRAY [1], [2]..
+function forwardPrintingWithSlice(arr, i = 0, j = 1) {
+  if (i === arr.length) return;
+
+  console.log(arr.slice(i, j));
+  forwardPrintingWithSlice(arr, i + 1, j + 1);
+}
